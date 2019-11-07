e73b63e579e971a1d7bbe761ec5c055d7b16c898
Albert B George <albertbgeorge@gmail.com>
2019-11-07 20:45:49 +0530

    Backend

--- adminhome.php
+++ adminhome.php
@@ -47,20 +47,18 @@ require_once("db.php");
           <div style="display: inline-block;">VIVA - LINK</div>
         </div>
         <nav>
-          <?php if(empty($_SESSION['user'])) { ?>
           <a href="index.php">Home</a>
-          <a href="admin_login.php">Admin login</a>
-          <?php } else if(isset($_SESSION['user'])) { ?>
-          <a href="question.php">Question</a>
+          <a href="ranklist.php">Ranklist</a>
           <a href="logout.php">Logout</a>
-        <?php } ?>
-          <a href="contact.html">Contact</a>
           <div class="animation start-home"></div>
         </nav>
       </div>
       <div class="red">
         <br>
-        <h2>Welcome <?php echo $_SESSION['name'] ?></h2>
+        <h2>Welcome Admin</h2>
+        <a href="add.php">Add Questions</a>
+        <a href="all.php">View All Questions</a>
+        <a href="add_stud.php">Add Student</a>
       </div>
       <div style="background: #39424e;height: 60px;"></div>
     </div>
--- login.php
+++ login.php
@@ -9,10 +9,11 @@ $query = "SELECT * FROM user WHERE username='$username' AND pass='$password'";
 $result = mysqli_query($conn, $query) or die($mysqli_error($conn));
 $num = mysqli_num_rows($result);
 if($num == 0) {
-  $error = $$_GET['error'];
-  $error = "<span class='red'>Incorrect Username or Password</span>";
-  header('location:login.php?error=' . $error);
+  echo "<script> alert('Username or Password wrong!'); window.location.href = 'index.php';</script>";
 } else {
+  $played_on = date('Y-m-d H:i:s');
+  $update = "UPDATE user SET played_on = '$played_on' WHERE username='$username'";
+  $runupdate = mysqli_query($conn, $update) or die(mysqli_error($conn));
   $row = mysqli_fetch_array($result);
   $_SESSION['user'] = $row['username'];
   $_SESSION['name'] = $row['name'];
