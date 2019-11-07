9059eb493f4bf45b1d8e3d2e68b54d8e6bbfbb6e
vpjashith <jashith.mec@gmail.com>
2019-11-08 04:04:47 +0530

    Merge https://github.com/vpjashith/Viva
    updates

--- account.php
+++ account.php
@@ -5,6 +5,7 @@ require_once("db.php");
 <!DOCTYPE html>
 <html lang="en">
   <title>Viva-Link &nbsp; Home</title>
+
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -16,25 +17,66 @@ require_once("db.php");
       type="image/png"
       href="images/subject-icon-12.jpg"
     />
+    <link rel="stylesheet" href="./css/logout_icon.css" />
     <link rel="stylesheet" href="css/box_design.css" />
+    <link rel="stylesheet" href="css/account.css" />
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
     <script>
       function fade() {
         $("#form").fadeOut(300);
         $(".wrapper").addClass("form-success");
       }
-    </script>
 
+      function show() {
+        if (document.getElementById("drop").style.opacity == 0) {
+          document.getElementById("drop").style.opacity = 1;
+          document.getElementById("tri").style.opacity = 1;
+        } else {
+          document.getElementById("drop").style.opacity = 0;
+          document.getElementById("tri").style.opacity = 0;
+        }
+      }
+      function w() {
+        if (document.getElementById("drop").style.opacity == 1) {
+          document.getElementById("drop").style.opacity = 0;
+          document.getElementById("tri").style.opacity = 0;
+        }
+      }
+    </script>
     <style>
       .red {
-        height: calc(100vh - 128px);
+        height: calc(100vh - 114px);
       }
     </style>
   </head>
 
   <body>
-    <div>
+    <div id="tri" class="triangle-up"></div>
+    <div class="sidebar" id="drop">
+      <img
+        src="images/perlog.png"
+        alt="person"
+        width="100px"
+        height="100px"
+        style="margin-left:65px; margin-top: 20px;"
+      />
+      <div class="nameo">
+        Jasthith V.P
+      </div>
+      <button class="logout">
+        Logout
+      </button>
+    </div>
+    <img
+      src="images/perlog.png"
+      alt="person"
+      width="30px"
+      height="30px"
+      class="photo"
+      onclick="show()"
+    />
+    <div onclick="w()">
       <div class="navi">
         <div class="logo">
           <img
@@ -47,22 +89,19 @@ require_once("db.php");
           <div style="display: inline-block;">VIVA - LINK</div>
         </div>
         <nav>
-          <?php if(empty($_SESSION['user'])) { ?>
-          <a href="index.php">Home</a>
-          <a href="admin_login.php">Admin login</a>
-          <?php } else if(isset($_SESSION['user'])) { ?>
-          <a href="question.php">Question</a>
+          <?php if (empty($_SESSION['user'])) { ?>
+          <a href="account.php">Home</a>
+          <a href="history.php">History</a>
+          <?php } else if (isset($_SESSION['user'])) { ?>
+
           <a href="logout.php">Logout</a>
-        <?php } ?>
-          <a href="contact.html">Contact</a>
+          <?php } ?>
+          <a href="html/account_contact.html">Contact</a>
           <div class="animation start-home"></div>
         </nav>
       </div>
-      <div class="red">
-        <br>
-        <h2>Welcome <?php echo $_SESSION['name'] ?></h2>
-      </div>
-      <div style="background: #39424e;height: 60px;"></div>
+      <div class="red"></div>
+      <div style="background: #39424e;height: 50px;"></div>
     </div>
   </body>
 </html>
